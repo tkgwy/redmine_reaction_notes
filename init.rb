eccf0d97cbf96413107ff8b99a0c4af22e90ce2b
Yosuke Takigawa <09asp.ip@gmail.com>
2017-07-26 16:50:42 +0900

    Init redmine plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_reaction_notes do
+  name 'Redmine Reaction Notes plugin'
+  author 'Yosuke Takigawa'
+  description 'This plugin provides reaction feature like Slack for notes.'
+  version '0.0.1'
+  url 'https://github.com/tkgwy/redmine_reaction_notes'
+  author_url 'https://github.com/tkgwy'
+end
