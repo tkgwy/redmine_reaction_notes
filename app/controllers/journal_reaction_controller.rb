ea8a4e09ea96315142d38b6149d939041d457a76
Yosuke Takigawa <09asp.ip@gmail.com>
2017-11-07 12:25:50 +0900

    Add create&delete server logic

--- app/controllers/journal_reaction_controller.rb
+++ app/controllers/journal_reaction_controller.rb
@@ -3,5 +3,15 @@ class JournalReactionController < ApplicationController
 
 
   def create
+    @journal = Journal.find(params[:journal_id])
+    reaction = @journal.journal_reactions.find_or_create_by(body: params[:body])
+    reaction_user = reaction.journal_reaction_users.find_or_initialize_by(user: User.current)
+    unless reaction_user.new_record? then
+      reaction_user.destroy
+    else
+      reaction_user.save!
+    end
+
+    reaction.destroy if reaction.journal_reaction_users.size == 0
   end
 end
